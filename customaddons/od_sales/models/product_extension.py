8ceaa491dcdf217de381f24e9be30827ee76888e
henriqueespadas <henriqueep@hotmail.com>
2022-08-13 00:35:19 -0300

    custom module development

--- customaddons/od_sales/models/product_extension.py
+++ customaddons/od_sales/models/product_extension.py
@@ -4,3 +4,5 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     computer_generation = fields.Char(string='Computer Generation')
+    warranty_start_date = fields.Date(string='Warranty start')
+    warranty_end_date = fields.Date(string='Warranty End')
