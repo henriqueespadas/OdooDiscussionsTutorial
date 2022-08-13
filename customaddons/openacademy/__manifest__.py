277d0b90e366d79819e12b7588ee9927e23f7aff
henriqueespadas <henriqueep@hotmail.com>
2022-08-13 01:02:42 -0300

    add new custom model

---
+++ customaddons/openacademy/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Sale extension',
+    'version': '13.0.01',
+    'author': 'Henrique Paranhos',
+    'website': 'Evaalexandrina.com.br',
+    'depends': ['sale'],
+    'data': [
+        'views/product_extension.xml'
+    ],
+}
